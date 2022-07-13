6bfd8dd9444f858a007f18e7be0b9f5f38b77c78
nuurfatimah13 <nuurfatimah13@gmail.com>
2022-07-13 17:11:52 +0700

    updated index.js

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 // run `node index.js` in the terminal
 
 console.log(`Hello Node.js v${process.versions.node}!`);
+console.log("Menyalakan mesin kopi");
+console.log("Menggiling biji kopi");
+console.log("Memanaskan air");
+console.log("Mencampurkan air dan kopi");
+console.log("Menuangkan kopi ke dalam gelas");
+console.log("Menuangkan susu ke dalam gelas");
+console.log("Kopi Anda sudah siap!");
