b2c765b545f57a94e2d090c17fc39174b1a0e4de
nuurfatimah13 <nuurfatimah13@gmail.com>
2022-07-13 17:39:02 +0700

    updated index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-// run `node index.js` in the terminal
-
-console.log(`Hello Node.js v${process.versions.node}!`);
-console.log("Menyalakan mesin kopi");
-console.log("Menggiling biji kopi");
-console.log("Memanaskan air");
-console.log("Mencampurkan air dan kopi");
-console.log("Menuangkan kopi ke dalam gelas");
-console.log("Menuangkan susu ke dalam gelas");
-console.log("Kopi Anda sudah siap!");
+const coffeeStock = require('./state');
+ 
+const makeCoffee = (type, miligrams) => {
+    if (coffeeStock[type] >= miligrams) {
+        console.log("Kopi berhasil dibuat!");
+    } else {
+        console.log("Biji kopi habis!");
+    }
+}
+ 
+makeCoffee("robusta", 80);
