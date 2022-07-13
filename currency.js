878b87ca2dedc1668bf5e1adfadadf4f620f33a8
nuurfatimah13 <nuurfatimah13@gmail.com>
2022-07-13 21:17:27 +0700

    add file currency.js

---
+++ currency.js
@@ -0,0 +1,6 @@
+// Fungsi setTimeout
+console.log("Selamat datang!");
+setTimeout(() => {
+  console.log("Terima kasih sudah mampir, silakan datang kembali!");
+}, 3000);
+console.log("Ada yang bisa dibantu?");
